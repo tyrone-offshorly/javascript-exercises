102af568774ae2a93b19b1e09efc655fab02f7fa
tyrone-offshorly <188826837+tyrone-offshorly@users.noreply.github.com>
2024-11-22 16:45:00 +0800

    Finish 05 calculator exeercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  const result =  array.reduce((accumulator, current) => {
+    return accumulator += current;
+  }, 0);
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  const result =  array.reduce((accumulator, current) => {
+    return accumulator *= current;
+  }, 1);
+  return result;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a ** b; 
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if(num === 0) {
+    return 1;
+  }
+  let result = 1;
+  for(let i = 1; i <= num; i++) {
+    result *= i;
+  }
+  return result;
+  
+
 };
 
 // Do not edit below this line
