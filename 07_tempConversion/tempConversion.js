70ab5346acb5851209286e6866b5c28f48aa3fcd
tyrone-offshorly <188826837+tyrone-offshorly@users.noreply.github.com>
2024-11-22 08:16:31 +0800

    Finish arrays and loop  exercises

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+  const toCelcius = (num - 32) * 5 / 9;
+  return Math.round(toCelcius * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+  const toFahrenheit = (num * 9 / 5) + 32;
+  return Math.round(toFahrenheit * 10) / 10;
 };
 
 // Do not edit below this line
