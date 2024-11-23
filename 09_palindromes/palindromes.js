3c7bad90c04e4734ee582600e888f9fb2e82e7e9
tyrone-offshorly <188826837+tyrone-offshorly@users.noreply.github.com>
2024-11-24 04:15:23 +0800

    Complete exercise 9 palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,21 @@
-const palindromes = function () {
+const palindromes = function (string) {
+// split the string
+// remove the spaces
+// convert everything to lowercase
+// set left to 0
+// set right to len(string) - 1
+// WHILE left < right
+//  IF string[left] !== string[right]
+//    RETURN false
+//    left++
+//    right++
+  const ALPHANUMERIC = "abcdefghijklmnopqrstuvwxyz0123456789"
+  const lowerString = string.toLowerCase().split('').filter((char) => ALPHANUMERIC.includes(char)).join('');
 
-};
+  const reversedString = lowerString.split('').reverse().join('');
+
+  return lowerString === reversedString;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
