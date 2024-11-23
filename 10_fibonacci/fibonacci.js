97e9fa242dc8b2c69914ed33295e5b717c5f1f1b
tyrone-offshorly <188826837+tyrone-offshorly@users.noreply.github.com>
2024-11-24 04:27:29 +0800

    Complete exercise 10 fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(input) {
+  const num = Number(input); 
 
+  if(num === NaN) {
+    throw('Input is not a number');
+  }
+  if(num < 0) {
+    return "OOPS"
+  }
+  if(num === 0 || num === 1) {
+    return num;
+  }
+  const arr = [0, 1];
+
+  for(let i = 2; i <= num; i++) {
+    const sum = arr[0] + arr[1];
+    arr.shift();
+    arr.push(sum);
+  }
+  return arr[1];
 };
 
 // Do not edit below this line
