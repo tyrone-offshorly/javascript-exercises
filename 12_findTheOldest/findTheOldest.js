ed0c671d4af904b9f58d90ddf3547939853897d1
tyrone-offshorly <188826837+tyrone-offshorly@users.noreply.github.com>
2024-11-25 10:16:47 +0800

    Complete exercise 12 find the oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
 
+  const d = new Date();
+  const year = d.getFullYear();
+  const oldest = people.reduce((oldestPerson, currentPerson) => {
+    const oldestLatest = oldestPerson.yearOfDeath || year; 
+    const currentLatest = currentPerson.yearOfDeath || year; 
+
+    const oldestAge = oldestLatest - oldestPerson.yearOfBirth;
+    const currAge = currentLatest - currentPerson.yearOfBirth;
+    if (currAge > oldestAge) {
+      return currentPerson;
+    } else {
+      return oldestPerson;
+    }
+  });
+  return oldest;
 };
 
 // Do not edit below this line
